3b0dc2b46c74a247ffd7df46ad1c187d2fa07092
Arex23 <garbuja12alson@gmail.com>
2019-08-08 09:22:25 +0545

    Added new js files, and modified hellojs.html

---
+++ 2nd_sem/javascript/fundamental/hello_operator.js
@@ -0,0 +1,10 @@
+// alert("1 2 3");
+
+var name = prompt("Enter name:");
+// prompt used to take input from user
+var num1 = parseInt(prompt("Enter number1:"));
+var num2 = (prompt("Enter number2:"));
+
+// parse change the variable to either int or float
+//  + operator add and concatinate
+// if two number and string is added then they are concatinated 
---
+++ 2nd_sem/javascript/fundamental/variable.js
@@ -0,0 +1,16 @@
+var a=200;
+var $new = "Hello";
+
+//  Variable name must start with a letter , _ or $
+//  subsequent number can be (0-9)
+// It is caseSensative i.e: A and a are different variable name
+
+const name = "Alson";
+
+// const variables can't have its value changed
+// And it have to have a value when declearing the varibale
+
+/*
+    The above comments are single-line
+    And this is multi-line
+*/
