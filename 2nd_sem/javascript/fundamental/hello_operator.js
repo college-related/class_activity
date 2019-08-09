645023007be074c9dd7222c32075afeea8587b2e
Arex23 <garbuja12alson@gmail.com>
2019-08-09 09:09:49 +0545

    small change

--- 2nd_sem/javascript/fundamental/hello_operator.js
+++ 2nd_sem/javascript/fundamental/hello_operator.js
@@ -3,7 +3,7 @@
 var name = prompt("Enter name:");
 // prompt used to take input from user
 var num1 = parseInt(prompt("Enter number1:"));
-var num2 = (prompt("Enter number2:"));
+var num2 = prompt("Enter number2:");
 
 // parse change the variable to either int or float
 //  + operator add and concatinate
