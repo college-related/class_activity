2902f674670a942022d1a4b092704bc0b3a4627c
Arex23 <garbuja12alson@gmail.com>
2019-08-12 09:44:41 +0545

    added loop file for js

---
+++ 2nd_sem/javascript/fundamental/loop.js
@@ -0,0 +1,19 @@
+var i;
+
+for(i=0; i<3; i++){
+    for(var j=0; j<2; j++){
+        console.log(j);
+    }
+}
+
+while(i<10)
+{
+    console.log(i);
+    i++;
+}
+
+do 
+{
+    console.log(j);
+    j++;
+}while(j<1);
