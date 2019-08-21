2a4e479e898c346ee177f6d2529618ddc74da0f5
Arex23 <garbuja12alson@gmail.com>
2019-08-21 10:08:53 +0545

    added a test array file

---
+++ 2nd_sem/javascript/fundamental/array.js
@@ -0,0 +1,26 @@
+var numbers = [1, 4, 5.5];
+// var name = new Array();
+
+// to add new elements in the array at the last
+numbers.push(5,7);
+
+// to delete last element
+numbers.pop();
+
+// to add elements at first of the array
+numbers.unshift(8, 6);
+
+// to delete the first item of the array
+numbers.shift();
+
+// to delete selected items from the aray
+numbers.splice(1, 2);
+
+console.log(numbers);
+
+numbers.find(2);
+
+/*
+    Array can have different data types in js
+    No need to declare the size of arrar in js
+*/
