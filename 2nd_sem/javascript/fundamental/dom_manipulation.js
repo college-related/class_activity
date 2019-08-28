bae63f7bc392840e14dc98fbbbe708c5d6e7274d
Arex23 <garbuja12alson@gmail.com>
2019-08-28 13:13:55 +0545

    some more added features

--- 2nd_sem/javascript/fundamental/dom_manipulation.js
+++ 2nd_sem/javascript/fundamental/dom_manipulation.js
@@ -1,15 +1,22 @@
 // interface between js and html+css =DOM
 
-var h = document.getElementsByTagName("h1");
+var h = document.querySelector("h1");
 h.style.color = "red";
 
 var b = document.getElementById("box");
 b.style.border = "2px solid";
 b.style.height = "200px";
 
-var bx = document.getElementsByClassName("boxes");
-bx.style.border = "1px solid";
-bx.style.height = "100px";
+// var bx = document.getElementsByClassName("boxes");
+// bx.style.border = "1px solid";
+// bx.style.height = "100px";
 
-document.querySelectorAll(".new_boxes").style.height = "100px";
-document.querySelectorAll(".new_boxes").style.border= "1px solid";
+var a = document.getElementById("none");
+a.style.color = "green";
+console.log(a.getAttribute("href"));
+
+function swap_color()
+{
+    h.style.color = "green";
+    a.style.color = "red";
+}
