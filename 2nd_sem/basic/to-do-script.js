6981045e3764ead4daed474b7f5c867faf9ef0dd
Arex23 <garbuja12alson@gmail.com>
2019-08-30 08:52:46 +0545

    unwanted code deleted and added function onkeydown

--- 2nd_sem/basic/to-do-script.js
+++ 2nd_sem/basic/to-do-script.js
@@ -3,5 +3,10 @@ function add()
     var uncompleteList = document.getElementById("list");
     var to_dos = document.getElementById("task").value;
     uncompleteList.innerHTML += "<input type='checkbox'>&nbsp;<li>"+to_dos+"</li><br/>";
-    i++;
 }
+
+document.getElementById("task").addEventListener("keydown", function(event){
+    if(event.keyCode == 13){
+        add();
+    }
+});
