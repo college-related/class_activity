828de14a43fe4beeb9268d3f271f8a0bf59aa871
Arex23 <garbuja12alson@gmail.com>
2019-08-31 14:53:48 +0545

    added the files to-do from lab assignment

---
+++ 2nd_sem/test/to-do-web.js
@@ -0,0 +1,48 @@
+var task = [];
+var ul = document.getElementById("list");
+var cUl = document.getElementById("cList");
+var to_dos = document.querySelector("#task");
+
+function add()
+{
+    if(to_dos.value){
+        task.push(
+            {
+                name: to_dos.value,
+                isComplete: false
+            }
+        );
+        to_dos.value = "";
+        render();
+    }
+}
+
+to_dos.addEventListener("keydown", function(event){
+        if(event.keyCode == 13){
+                add();
+        }
+});
+
+function changeStatus(task1)
+{
+    if(task1.isComplete == false){
+        task1.isComplete = true;
+    }else{
+        task1.isComplete = false;
+    }
+}
+
+function render(){
+    var completed = "";
+    var incomplete = "";
+    task.forEach(function(t, ind){
+        if(t.isComplete){
+            completed += "<li><input type='checkbox' onclick = 'changeStatus(task["+ ind + "]); render() ;' >" + t.name + "</li><br/>" ;
+        }else{
+            incomplete += "<li><input type='checkbox' onclick = 'changeStatus(task["+ ind + "]); render() ;' >" + t.name + "</li><br/>" ;
+        }
+    })
+    ul.innerHTML = incomplete ;
+    cUl.innerHTML = completed ;
+}
+
